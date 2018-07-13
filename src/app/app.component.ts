54f8fd88d7c05e50c6bd8b7aa801573527aa2361
Jonathan Wilson <jon.wilsond@gmail.com>
2018-07-13 15:58:03 -0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = "Jonathan's App";
+  sometext = 'My Text';
+
+  onclick(): void {
+  	this.sometext = "This is some text."
+  }
 }
