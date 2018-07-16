f42056a68171ef6fcd6c54be8dccc037e3c3193a
Jonathan Wilson <jon.wilsond@gmail.com>
2018-07-16 12:29:52 -0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = "Jonathan's App";
-  sometext = 'My Text';
+  title: string = 'Jon';
+  sometext = 'my text';
 
+  displayName: boolean = true;
+
+  fv = "FV";
+  fnv = "FNV";
+  mv = "MV";
+  mnv = "MNV";
+
+  names = [
+    { name: "Michael",   tag: 4, abool: true },
+    { name: "Dan",       tag: 3, abool: false }, 
+    { name: "Anna",      tag: 1, abool: true },
+    { name: "Marcus",    tag: 4, abool: false },
+    { name: "Foster",    tag: 4, abool: true },
+    { name: "Jonathan",  tag: 4, abool: false },
+    { name: "Larry",     tag: 3, abool: true }
+  ];
+
+  toggle(): void {
+    this.displayName = !this.displayName;
+  }
   onclick(): void {
-  	this.sometext = "This is some text."
+    this.sometext = "This is some text";
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserCreateComponent } from './user/user-create/user-create.component';
+import { UserDetailComponent } from './user/user-detail/user-detail.component';
+import { UserEditComponent } from './user/user-edit/user-edit.component';
+import { UserListComponent } from './user/user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserCreateComponent,
+    UserDetailComponent,
+    UserEditComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule
